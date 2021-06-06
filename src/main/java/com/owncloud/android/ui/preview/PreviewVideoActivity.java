4c141e8823094e981261c62152760751890166e7
Chris Narkiewicz <hello@ezaquarii.com>
2021-06-06 16:59:31 +0100

    Removed Account dependency from preview fragments
    Remove Account dependency in text preview fragment.
    Remove Account dependency from outdated JavaDoc in
    image and video preview fragments.
    
    Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

--- src/main/java/com/owncloud/android/ui/preview/PreviewVideoActivity.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewVideoActivity.java
@@ -20,7 +20,6 @@
 
 package com.owncloud.android.ui.preview;
 
-import android.accounts.Account;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -68,16 +67,6 @@ public class PreviewVideoActivity extends FileActivity implements OnCompletionLi
     private MediaController mMediaController;   // panel control used by the user to control the playback
     private Uri mStreamUri;
 
-    /**
-     *  Called when the activity is first created.
-     *
-     *  Searches for an {@link OCFile} and ownCloud {@link Account} holding it in the starting {@link Intent}.
-     *
-     *  The {@link Account} is unnecessary if the file is downloaded; else, the {@link Account} is used to
-     *  try to stream the remote file - TODO get the streaming works
-     *
-     *  {@inheritDoc}
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
